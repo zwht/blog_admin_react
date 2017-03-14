9f3d0436d23cabaf7a32ab20c6040ff99296fd99
zhaowei <1512763623@qq.com>
2017-03-14 18:24:34 +0800

    good

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ var config = {
     output: {
         path: path.resolve(__dirname, './build'),
         filename: '/assets/js/bundle.js',
-        publicPath: "http://127.0.0.1:9999/"
+        publicPath: "http://127.0.0.1:8889/"
     },
 
     devServer: {
